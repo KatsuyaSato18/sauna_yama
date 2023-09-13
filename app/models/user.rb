04aef538ca19ab414def8a8b8af4d12926d475f7
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-09-13 12:10:07 +0000

    9.13

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  validates :name, presence: true
+  validates :telephone_number, presence: true
+
+end
