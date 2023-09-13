8fd80a735c925392e5bd1182769832cc1b2548a1
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-09-13 14:52:38 +0000

    9.13-1

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
+  has_one_attached :image
+
 end
--- db/migrate/20230912142003_create_posts.rb
+++ db/migrate/20230912142003_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.string :sauna_name, null: false
+      t.string :address, null: false
+      t.text :caption, null: false
+      t.integer :status, default: 0, null: false
       t.timestamps
     end
   end
