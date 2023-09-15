fb238eda0dc4cf02c726e8a73ed994db57d35310
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-09-15 13:37:38 +0000

    9.15-3

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,12 @@
 class Admin::HomesController < ApplicationController
+  before_action :authenticate_admin!
   def top
+  end
+
+  private
+  def authenticate_admin!
+    unless admin_signed_in?  # Devise提供のヘルパー
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  before_action :reject_admin, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,23 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+   protected
+
+
+  def reject_admin
+    if params[:admin][:email].blank? || params[:admin][:password].blank?
+      flash[:notice] = "項目を入力してください。"
+      return
+    end
+    @admin = Admin.find_by(email: params[:admin][:email])
+    if @admin
+      unless @admin.valid_password?(params[:admin][:password])
+        flash[:notice] = "パスワードが違います。"
+      end
+    else
+      flash[:notice] = "該当する管理者が見つかりません。再度入力してください。"
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
