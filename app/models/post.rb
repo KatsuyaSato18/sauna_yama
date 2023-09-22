bf3d429406e544bf19e32ad12d1851686c8bd251
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-09-22 14:14:25 +0000

    9.22

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :post_tags, dependent: :destroy
   has_one_attached :image
+  validates :image, presence: true
+
+   def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,71 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create(email: "a@a", password: "aaaaaa")
+puts "seedの実行を開始"
+
+Admin.find_or_create_by!(email: "a@a") do |admin|
+  admin.password = "aaaaaa"
+end
+
+nana = User.find_or_create_by!(email: "nana@example.com") do |user|
+  user.name = "Nana"
+  user.password = "password"
+  user.telephone_number = "00000000000"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+end
+
+jame = User.find_or_create_by!(email: "jame@example.com") do |user|
+  user.name = "Jame"
+  user.password = "password"
+  user.telephone_number = "00000000000"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
+end
+
+luca = User.find_or_create_by!(email: "luca@example.com") do |user|
+  user.name = "Luca"
+  user.password = "password"
+  user.telephone_number = "00000000000"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
+end
+
+adam = User.find_or_create_by!(email: "adam@example.com") do |user|
+  user.name = "Adam"
+  user.password = "password"
+  user.telephone_number = "00000000000"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")
+end
+
+Post.find_or_create_by!(title: "湖を一望しながら整う") do |post|
+  post.sauna_name = "Biwako Sauna"
+  post.address = "滋賀県高島市マキノ町海津"
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post.caption = "湖に囲まれながらサウナ！湖に飛び込んで、自然の水風呂最高！"
+  post.user = Nana
+end
+
+Post.find_or_create_by!(title: "雪サウナ") do |post|
+  post.sauna_name = "スノーサウナ"
+  post.address = "長野県東御市和"
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
+  post.caption = "冬限定の大自然！長野の雪景色"
+  post.user = Jame
+end
+
+Post.find_or_create_by!(title: "迫力感がえぐ！") do |post|
+  post.sauna_name = "やまほっと"
+  post.address = "宮城県加美郡加美町"
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
+  post.caption = "目の前が山って、こんなサウナ見たことある？"
+  post.user = Luca
+end
+
+Post.find_or_create_by!(title: "日本最北端！？") do |post|
+  post.sauna_name = "ほっととのう"
+  post.address = "北海道稚内市"
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename:"sample-post4.jpg")
+  post.caption = "日本の北のサウナは南国のようなホットな場所だった。"
+  post.user = Adam
+end
+
+puts "seedの実行が完了しました"
+
