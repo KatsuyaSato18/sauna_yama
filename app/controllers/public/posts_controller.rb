f354d08f752eb46e0fcaad296fca276a25d8e370
EC2 Default User <129525791+KatsuyaSato18@users.noreply.github.com>
2023-09-23 07:48:41 +0000

    9.23

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,47 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "投稿に成功しました."
+      redirect_to posts_path
+    else
+      flash[:notice] = "投稿に失敗しました."
+      render :new
+    end
   end
 
   def index
+    @post = Post.page(params[:page])
   end
 
   def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 
   def edit
+    @post = Post.find(params[:id])
+    if @post.user != current_user
+      redirect_to posts_path
+    end
   end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :sauna_name, :adress,:image, :caption, :status)
+  end
+
 end
